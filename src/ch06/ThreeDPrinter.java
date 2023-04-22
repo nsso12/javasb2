d166e349ac86e3d1497b8b3842c291c6813b0ec1
nsso12 <sora628889@gmail.com>
2023-04-22 15:28:50 +0900

    update

---
+++ src/ch06/ThreeDPrinter.java
@@ -0,0 +1,14 @@
+package ch06;
+
+public class ThreeDPrinter {
+    private Object material;
+
+    public void setMaterial(Object material) {
+        this.material = material;
+    }
+
+    public Object getMaterial() {
+        return material;
+    }
+
+}
---
+++ src/ch06/ThreeDPrinter2.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class ThreeDPrinter2 {
+    private Plastic material;
+
+    public void setMaterial(Plastic mateiral) {
+        this.material = mateiral;
+    }
+
+    public Plastic getMaterial() {
+        return material;
+    }
+}
