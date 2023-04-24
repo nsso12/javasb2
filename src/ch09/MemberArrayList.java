37edd9a3c345735a189ab39f690cede12bef57a6
nsso12 <sora628889@gmail.com>
2023-04-24 14:54:14 +0900

    update

---
+++ src/ch09/MemberArrayList.java
@@ -0,0 +1,35 @@
+package ch09;
+
+import java.util.ArrayList;
+
+public class MemberArrayList {
+
+    private ArrayList<Member> arrayList; // ArrayList 선언
+    public MemberArrayList() {
+        arrayList = new ArrayList<Member>();  // 멤버로 선언한 ArrayList 생성
+    }
+
+    public void addMember(Member member) {  //ArrayList에 멤버 추가
+        arrayList.add(member);
+    }
+
+    public boolean removeMember(int memberId) { // 멤버 아이디를 매개변수로, 삭제 여부를 반환
+        for (int i = 0; i < arrayList.size(); i++) { // 해당 아이디를 가진 멤버를 list에서 찾음
+            Member member = arrayList.get(i);
+            int tempId = member.getMemberId();
+            if (tempId == memberId) {  // 멤버 아이디가 매개변수와 일치하면
+                arrayList.remove(i);  // 해당 멤버를 삭제
+                return true;  // true를 반환
+            }
+        }
+        System.out.println(memberId + "가 존재하지 않습니다");  //for가 끝날 때까지 return이 안된 경우
+        return false;
+    }
+
+    public void showAllMember() {
+        for(Member member : arrayList) {
+            System.out.println(member);
+        }
+        System.out.println();
+    }
+}
---
+++ src/ch09/MemberArrayListTest.java
@@ -0,0 +1,24 @@
+package ch09;
+
+public class MemberArrayListTest {
+    public static void main(String[] args) {
+
+        MemberArrayList memberArrayList = new MemberArrayList();
+
+        Member member1 = new Member(1001, "진경");
+        Member member2 = new Member(1002, "소현");
+        Member member3 = new Member(1003, "지민");
+        Member member4 = new Member(1004, "효정");
+
+        memberArrayList.addMember(member1);
+        memberArrayList.addMember(member2);
+        memberArrayList.addMember(member3);
+        memberArrayList.addMember(member4);
+
+        memberArrayList.showAllMember();
+
+        memberArrayList.removeMember((member1.getMemberId()));
+        memberArrayList.showAllMember();
+
+    }
+}
