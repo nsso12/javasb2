f7e3d6d8971ce26348a3e056e7a1a9ba554fb961
nsso12 <sora628889@gmail.com>
2023-04-25 19:01:45 +0900

    update

---
+++ src/ch11/TestMyNumber.java
@@ -0,0 +1,8 @@
+package ch11;
+
+public class TestMyNumber {
+    public static void main(String[] args) {
+        MyNumber max =  (x, y)->(x>= y)? x:y;
+        System.out.println(max.getMax(10,20));
+    }
+}
