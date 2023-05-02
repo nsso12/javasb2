d6dd2c0bf7273ca46171708ea7d8d393f886377f
nsso12 <sora628889@gmail.com>
2023-05-02 12:30:42 +0900

    update

---
+++ src/ch13/AutoCloseTest.java
@@ -0,0 +1,13 @@
+package ch13;
+
+public class AutoCloseTest {
+    public static void main(String[] args) {
+
+        AutoCloseObj obj = new AutoCloseObj();
+        try (obj) {
+            throw new Exception();
+        } catch(Exception e) {
+            System.out.println("예외 부분입니다");
+        }
+    }
+}
